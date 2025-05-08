647560f9d229eed5a75c67f4280bef572d4e4824
liam woodward <liamwoodward20@outlook.com>
2025-05-08 16:06:22 +0200

    adding to github

---
+++ app.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display')
+const numButtons = document.querySelectorAll('.number-btns')
+
+
+function displayOutput(){
+  const displayInput = document.getElementById('display');
+  display.value += button.textContent;
+
+}
+
+
+function numInput(){
+  numButtons.addEventListener('click', function(){
+    for( const button of numButtons) {
+    
+    }
+   });
+  }
+
+
+displayOutput()
+
